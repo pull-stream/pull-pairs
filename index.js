a42eb463c51c0f60093a9aa4e1a2e625f210a3af
Dominic Tarr <dominic.tarr@gmail.com>
2014-09-24 00:43:22 +0300

    first in [null, 1], last is [[N, null]

--- index.js
+++ index.js
@@ -2,18 +2,23 @@ var looper = require('looper')
 
 module.exports = function (test) {
 
-  var prev = null
+  var prev = null, ended
 
   return function (read) {
 
     return function (abort, cb) {
-
+      if(ended) return cb(ended)
       //looper makes RangeErrors not happen.
       looper(function () {
         var next = this
         read(abort, function (end, _data) {
-          if(end) return cb(end)
-
+          if(end) {
+            data = test(prev, null)
+            ended = end
+            if(data !== undefined) cb(null, data)
+            else cb(end)
+            return
+          }
           var data = test(prev, _data)
 
           prev = _data
